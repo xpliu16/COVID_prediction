6b008d0791185219a3bdecc392207528a851620c
Xiao-Ping Liu <xpliu16@gmail.com>
2021-12-09 13:59:55 -0500

    VECM and additional tests and figures

--- COVID_get_data.R
+++ COVID_get_data.R
@@ -18,7 +18,7 @@ for (words in search_terms) {
     print(paste(s,words))
     gtrends(keyword = c(words), 
             geo = c(s),
-            time = "2016-12-6 2021-12-6") -> result1
+            time = "2016-12-8 2021-12-8") -> result1
     result <- result1$interest_over_time
     nrows = length(result$hits)
     temp1[ind:(ind+nrows-1)] <- paste0(as.Date(result$date))
@@ -34,4 +34,3 @@ df <- data.frame(t(rbind(temp1,temp3,temp4,temp2)))
 colnames(df) <- c("date","search_term","state","hits")
 
 write.csv(df,"C:\\Users\\Ping\\Desktop\\Project\\COVID_prediction\\data\\gtrend_data.csv", row.names = FALSE)
-# normalization
